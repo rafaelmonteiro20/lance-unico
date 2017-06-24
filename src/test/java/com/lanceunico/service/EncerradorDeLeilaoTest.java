4e7e22a2e703199533d609967f3b120bed0be5e0
Rafael Monteiro <rafajapones@gmail.com>
2017-06-24 11:23:30 -0300

    Adicionando mais testes para o Encerrador de leiloes.

--- src/test/java/com/lanceunico/service/EncerradorDeLeilaoTest.java
+++ src/test/java/com/lanceunico/service/EncerradorDeLeilaoTest.java
@@ -1,7 +1,6 @@
 package com.lanceunico.service;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
 import java.time.LocalDate;
@@ -40,6 +39,65 @@ public class EncerradorDeLeilaoTest {
 		assertTrue(leilao1.isEncerrado());
 		assertTrue(leilao2.isEncerrado());
 		assertEquals(2, encerrador.getTotal());
+	}
+	
+	@Test
+	public void naoDeveEncerrarLeiloesQueComecaramHaMenosDeUmaSemana() {
+		LocalDate data1 = LocalDate.now().minusDays(10);
+		LocalDate data2 = LocalDate.now().minusDays(5);
+		
+		
+		Leilao leilao1 = new LeilaoBuilder().para("Geladeira")
+							.naData(data1)
+							.build();
+		
+		Leilao leilao2 = new LeilaoBuilder().para("Fogão")
+							.naData(data2)
+							.build();
+		
+		List<Leilao> leiloes = Arrays.asList(leilao1, leilao2);
+		
+		LeilaoDAO dao = mock(LeilaoDAO.class);
+		when(dao.correntes()).thenReturn(leiloes);
+		
+		EncerradorDeLeilao encerrador = new EncerradorDeLeilao(dao);
+		encerrador.encerrar();
+		
+		assertTrue(leilao1.isEncerrado());
+		assertFalse(leilao2.isEncerrado());
+		assertEquals(1, encerrador.getTotal());
+	}
+	
+	@Test
+	public void naoDeveEncerrarLeiloesCasoNaoHajaNenhum() {
+		List<Leilao> leiloes = Arrays.asList();
+		
+		LeilaoDAO dao = mock(LeilaoDAO.class);
+		when(dao.correntes()).thenReturn(leiloes);
+		
+		EncerradorDeLeilao encerrador = new EncerradorDeLeilao(dao);
+		encerrador.encerrar();
+		
+		assertEquals(0, encerrador.getTotal());
+	}
+	
+	@Test
+	public void deveAtualizarLeiloesEncerrados() {
+		LocalDate data = LocalDate.now().minusDays(10);
+		
+		Leilao leilao = new LeilaoBuilder().para("Geladeira")
+						    .naData(data)
+							.build();
+		
+		List<Leilao> leiloesAntigos = Arrays.asList(leilao);
+		
+		LeilaoDAO dao = mock(LeilaoDAO.class);
+		when(dao.correntes()).thenReturn(leiloesAntigos);
+		
+		EncerradorDeLeilao encerrador = new EncerradorDeLeilao(dao);
+		encerrador.encerrar();
+		
+		verify(dao, times(1)).atualizar(leilao);
 	}
 	
 }
